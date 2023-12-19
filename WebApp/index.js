c4001601b6e38e01ddae8c46645963fbac732098
El Guerrito <elguerrrito@davids-mbp.mynetworksettings.com>
2023-12-18 20:46:00 -0500

    Created Add/Remove Item from LocSto feature

--- WebApp/index.js
+++ WebApp/index.js
@@ -1,15 +1,12 @@
 /*
 [] write WPA To-Do List App for this project
 [] save to loclalStorage 
-    [] set the key assigner for values
-        []get the last key stored in locSto 
-        []after remove check if locSto = null 
-        []increment the ID# after appending ; do none when remove() called 
-    [] test for its functionality
     [] show items on the screen upon appending
+[] add Enter functionality
+    - when user clicks Enter key the item is added to the list
 [] rewrite functions for ES6 standard
 [] add multiplier to items
-[] add easterEgg for pasta
+[] create an object that would store the input's value and ID 
 */
 
 const addBtn = document.getElementById("add-button");
@@ -18,8 +15,9 @@ const listEl = document.getElementById("shopping-list");
 const imgEl = document.querySelector("img")
 const originalImagePath = "assets/girl-rmbg.png"
 const easterEggImagePath = "assets/ramen_girl.png";
+let locStoIsEmpty = localStorage.length == 0; // not sure how it will be modified in the program 
 const getIDnumber = () => {
-    if(localStorage.length == 0){ // check if locSto is null; yes - we set IDnum to 0, no - set IDnum to last+1 key number
+    if(locStoIsEmpty){ // check if locSto is null; yes - we set IDnum to 0, no - set IDnum to last+1 key number
             
         console.log("Local Storage is null");
         return 0;
@@ -28,20 +26,28 @@ const getIDnumber = () => {
         return localStorage.length;
     }
 }
+const inputItem = {
+    "value": "",
+    "ID" : null
+}
+let globalItemIDNumber = getIDnumber();
 
-let itemID = getIDnumber();
+const push = (database, value) => { // pushes value to the localStorage and returns its ID number in localStorage
+    globalItemIDNumber++; 
+    database.setItem(globalItemIDNumber, value);
+    return globalItemIDNumber;
+}
 
-const push = (database, value) => {
-    itemID++; 
-    database.setItem(itemID, value);
+const remove = (itemID) => {
+    localStorage.removeItem(itemID);
+    callUpdateLocSto();
 }
 
 
 addBtn.addEventListener("click", function () {
-    let inputValue  = inputEl.value;
-    push(localStorage, inputValue);
-    addNewItem()
-    console.log(inputValue);
+    inputItem.value  = inputEl.value;
+    inputItem.ID = push(localStorage, inputItem.value);
+    addNewItem(inputItem);
     clearInputFieldReference();
 })
 
@@ -52,52 +58,52 @@ function clearListEl (){
     
     listEl.innerHTML = ""; //clearing list before updating data
 }
-/*
-RE-write for LocalStorage 
-    onValue(shoppingListInDB, function (snapshot) {
-
-        clearListEl();
+ 
+ const callUpdateLocSto = () => {
+    console.log("In call Update function"); //<--- WARNING LOG
+    clearListEl();
         
-        if(snapshot.exists()){
-            const shoppingArray = Object.entries(snapshot.val()); // return object and get values from that object and turn them in to array
-            for(let currentItem of shoppingArray){
+    if(locStoIsEmpty){
+        console.log(locStoIsEmpty);
+        const shoppingArray = Object.entries(localStorage); // return object and get values from that object and turn them in to array
+        for(let currentItem of shoppingArray){
 
-                let currentItemID = currentItem[0];
-                let currentItemValue = currentItem[1]
-                
-                if(currentItemValue.toLowerCase() === 'ramen') {
+            let currentItemID = currentItem[0];
+            let currentItemValue = currentItem[1]
+            
+            if(currentItemValue.toLowerCase() === 'ramen') {
 
-                    imgEl.src = easterEggImagePath;
-                }
-
-                console.log("ID:" + currentItemID);
-                console.log("Value:" + currentItemValue);
-
-                addNewItem(currentItem);
+                imgEl.src = easterEggImagePath;
             }
-        }else {
-            imgEl.src = originalImagePath;
-            listEl.innerHTML = "No items here... yet :)"
-        }
 
-    })
-*/
+            console.log("ID:" + currentItemID); //<--- WARNING LOG
+            console.log("Value:" + currentItemValue); //<--- WARNING LOG
+
+            addNewItem(currentItem);
+        }
+    }else {
+        imgEl.src = originalImagePath;
+        listEl.innerHTML = "No items here... yet :)"
+    }
+
+}
+
 function addNewItem(item){
 
     const li = document.createElement("li");
-    const itemID = item[0];
-    const itemValue = item[1];
+    const itemID = item.ID;
+    console.log("This is items's ID = " + item.ID)
+    const itemValue = item.value;
+    console.log("This is items's value = " + item.value)
 
     li.append(itemValue);
     listEl.append(li);
 
 // Rewrite for LOCALSTORAGE
-    // li.addEventListener("dblclick", function () {       
-    //     const itemRef = ref(database, `ShoppingList/${itemID}`);
+    li.addEventListener("dblclick", function () {     
 
-    //     remove(itemRef);
-    // })
-
-
-
+        remove(itemID);
+    })
 }
+
+callUpdateLocSto();
