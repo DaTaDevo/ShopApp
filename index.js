fddb64ddafb42a2a2be97ac1c48e2b1613a57ee6
El Guerrito <elguerrrito@davids-mbp.mynetworksettings.com>
2023-12-22 11:57:34 -0500

    Break down callUpdateLocSto() method

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const addBtn = document.getElementById("add-button");
-const inputEl = document.getElementById("input-field");
-const listEl = document.getElementById("shopping-list");
+const addBtn = document.querySelector("#add-button");
+const inputEl = document.querySelector("#input-field");
+const listEl = document.querySelector("#shopping-list");
 const imgEl = document.querySelector("img")
+const countEl = document.querySelector("#item-count")
 const originalImagePath = "assets/girl-rmbg.png"
 const easterEggRamenImagePath = "assets/ramen_girl.png";
 // ADD NEW EASTER EGGG FOR NOODELS PATH TO IMAGE HERE 
@@ -46,23 +47,69 @@ function clearListEl (){
     
     listEl.innerHTML = ""; //clearing list before updating data
 }
- 
+
+/*
+
+updateList() {
+
+  clearList() -> parseArray(array, ...methods) -> array.item => checkEasterEgg()  - check if any item triggers EasterEgg
+                                                                addIconToItem() - IP
+                                                                addItemToList() - add item to the list 
+                                                                showItemCount() - shows how many items in the list
+                                                
+
+}
+
+*/
+
+
+/*
+
+
+
+
+
+const checkEasterEgg = (const itemValue) => {
+    for (const easterTag of easterTags["tag"]){
+
+    }
+}
+
+const showItemCount = () => {
+
+}
+
+
+
+const updateList = () => {
+
+    clearListEl(); 
+
+    parseArray(Object.entries(localStorage), checkEasterEgg(), addItemToList());
+
+    showItemCount(globalIDNumber);
+
+}
+
+
+*/
+
 function callUpdateLocSto() { // calls LocalStorage to update the list
 
 
     clearListEl();
         
-    if(!locStoIsEmpty()){
+    if(!locStoIsEmpty()){ // check if localStorage is empty 
 
         console.log("callUpdateLocSto(): LS length is " + localStorage.length);
 
-        const shoppingArray = Object.entries(localStorage); // gets all elements from localStorage to our array
-        for(let currentItem of shoppingArray){
+        const shoppingArray = Object.entries(localStorage); // copy localStorage to local array
+        for(let currentItem of shoppingArray){ // parse through every element
 
-            let currentItemID = currentItem[0];
+            let currentItemID = currentItem[0]; 
             let currentItemValue = currentItem[1]
             
-            if(currentItemValue.toLowerCase() === 'ramen') {
+            if(currentItemValue.toLowerCase() === 'ramen') { // check if any is EasterEgg
                 imgEl.src = easterEggRamenImagePath;
             
             } else if (currentItemValue.toLowerCase() === 'noodles') {
@@ -72,25 +119,25 @@ function callUpdateLocSto() { // calls LocalStorage to update the list
 
             console.table(currentItem)
 
-            addNewItem(currentItemID, currentItemValue);
+            addItemToList(currentItemID, currentItemValue); //add item to list to display
         }
 
-    } else {
+    } else { // if localStorage is empty shows message 
             console.log("callUpdateLocSto(): LS length is " + localStorage.length);
-            imgEl.src = originalImagePath;
-            listEl.innerHTML = "No items here, yet :)"
+            imgEl.src = originalImagePath; //if any changes were made by EasterEgg images, it reverts it back.
+            listEl.innerHTML = "No items here, yet :)"// the message 
         }
 
 }
 
-function addNewItem(itemID, itemValue){
+function addItemToList(itemID, itemValue){
 //Capitalize itemValue
     itemValue = itemValue.charAt(0).toUpperCase() + itemValue.slice(1).toLowerCase();
 
     const li = document.createElement("li");
     //CHECKING FOR PASSED VALUES 
-    console.log("addNewItem(): Passed ID = " + `${itemID}`);
-    console.log("addNewItem(): Passed value = " + `${itemValue}`);
+    console.log("addItemToList(): Passed ID = " + `${itemID}`);
+    console.log("addItemToList(): Passed value = " + `${itemValue}`);
 
     li.append(itemValue);
     listEl.append(li);
